6d03a71a3d6a7299b08c3d8ce01dc3a19fb5f54f
KUMAR RISHAB <78472503+rishab0902@users.noreply.github.com>
2022-12-30 02:08:23 +0530

    Added solution - LeetHub

---
+++ Triangle Path Sum - GFG/triangle-path-sum.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  //recursive
+//   int f(int i, int j, vector<vector<int> > &triangle,int n) {
+//   if(i==n-1) return triangle[i][j];
+    
+//   int down = triangle[i][j]+f(i+1,j,triangle,n);
+//   int diagonal = triangle[i][j]+f(i+1,j+1,triangle,n);
+  
+//   return min(down, diagonal);
+  
+// }
+
+  //memo
+  
+int f(int i, int j, vector<vector<int> > &triangle,int n, 
+    vector<vector<int> > &dp) {
+  
+    if(dp[i][j]!=-1)
+    return dp[i][j];
+  
+    if(i==n-1) return triangle[i][j];
+    
+    int down = triangle[i][j]+f(i+1,j,triangle,n,dp);
+    int diagonal = triangle[i][j]+f(i+1,j+1,triangle,n,dp);
+  
+    return dp[i][j] = min(down, diagonal);
+  
+    }
+  
+int minimizeSum(int n, vector<vector<int>>& triangle) {
+        // Code here
+        vector<vector<int> > dp(n,vector<int>(n,-1));
+    return f(0,0,triangle,n,dp);
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> triangle;
+
+        for (int i = 0; i < n; i++) {
+            vector<int> temp;
+            for (int j = 0; j <= i; j++) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            triangle.push_back(temp);
+        }
+        Solution obj;
+        cout << obj.minimizeSum(n, triangle) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
