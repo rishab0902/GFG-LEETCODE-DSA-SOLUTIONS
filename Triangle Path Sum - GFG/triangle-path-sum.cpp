9594d52a28cf81def9dc544c426624fad66d8932
KUMAR RISHAB <78472503+rishab0902@users.noreply.github.com>
2022-12-30 13:29:04 +0530

    Added solution - LeetHub

--- Triangle Path Sum - GFG/triangle-path-sum.cpp
+++ Triangle Path Sum - GFG/triangle-path-sum.cpp
@@ -34,9 +34,26 @@ int f(int i, int j, vector<vector<int> > &triangle,int n,
     }
   
 int minimizeSum(int n, vector<vector<int>>& triangle) {
+    vector<vector<int> > dp(n,vector<int>(n,0));
+    
+    for(int j=0;j<n;j++){
+        dp[n-1][j] = triangle[n-1][j];
+    }
+    
+    for(int i=n-2; i>=0; i--){
+        for(int j=i; j>=0; j--){
+            
+            int down = triangle[i][j]+dp[i+1][j];
+            int diagonal = triangle[i][j]+dp[i+1][j+1];
+            
+            dp[i][j] = min(down, diagonal);
+        }
+    }
+    
+    return dp[0][0];
         // Code here
-        vector<vector<int> > dp(n,vector<int>(n,-1));
-    return f(0,0,triangle,n,dp);
+    //     vector<vector<int> > dp(n,vector<int>(n,-1));
+    // return f(0,0,triangle,n,dp);
         
     }
 };
