c6430d00634e259ead17a646fd406df55cc2d2a0
KUMAR RISHAB <78472503+rishab0902@users.noreply.github.com>
2022-12-31 14:12:54 +0530

    Time: 4 ms (34.86%), Space: 6.4 MB (52.21%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string x) {
+         stack<char> s;
+     for(int i=0;i<x.size();i++){
+         if(x[i]=='(' || x[i]=='{' || x[i]=='['){
+             s.push(x[i]);
+         }
+         else{
+             if(!s.empty() && x[i]==')' && s.top()=='('){
+                 s.pop();
+                 
+             }
+            else if(!s.empty() && x[i]==']' && s.top()=='['){
+                 s.pop();
+                 
+             }
+             else if(!s.empty() && x[i]=='}' && s.top()=='{'){
+                 s.pop();
+                 
+             }
+             else return false;
+             
+         }
+         
+     }
+     
+     if(s.empty())return true;
+     else return false;
+    }
+};
