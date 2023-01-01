6089284a3020dd2968fc4640b2801dbd636e5032
KUMAR RISHAB <78472503+rishab0902@users.noreply.github.com>
2023-01-01 20:39:32 +0530

    Time: 122 ms (5.34%), Space: 20.2 MB (70.93%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& f, int n) {
+        int cnt=0;
+        int x=f.size();
+        
+        if(n==0)return true;
+        
+        if(x==1){
+            if(!f[0])cnt++;
+           return (cnt==n);
+        }
+        
+        if(f[0]==0 && f[1]==0){
+            f[0]=2;
+            cnt++;
+        }
+        for(int i=1;i<x-1;i++){
+            if(f[i]==0){
+                if(f[i-1]==0 && f[i+1]==0){
+                    f[i]=2;
+                    cnt++;
+                }
+            }
+        }
+           if(!f[x-1] && !f[x-2]){
+               f[x-1]=2;
+               cnt++;
+           }
+           // return(cnt>=n);
+        for(int i=0;i<x;i++){
+            cout<<f[i]<<" ";
+        }
+        return(cnt>=n);
+           
+    }
+};
