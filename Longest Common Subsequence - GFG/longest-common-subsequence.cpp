1921ff107b1cff232431ad7ceef618a8ba454706
KUMAR RISHAB <78472503+rishab0902@users.noreply.github.com>
2023-01-11 12:51:25 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -21,8 +21,27 @@ class Solution
     {
         // your code here
         // int dp[x][y];
-        vector<vector<int>> dp(x,vector<int>(y,-1));
-        return f(x-1,y-1,s1,s2,dp);
+        // vector<vector<int>> dp(x,vector<int>(y,-1));
+           vector<vector<int>> dp(x+1,vector<int>(y+1,-1));
+        for(int i=0;i<=x;i++){
+        dp[i][0] = 0;
+    }
+    for(int j=0;j<=y;j++){
+        dp[0][j] = 0;
+    }
+      
+             
+   for(int i=1;i<=x;i++){
+       for(int j=1;j<=y;j++){
+           if(s1[i-1]==s2[j-1]){
+               dp[i][j]=1+dp[i-1][j-1];
+           }
+           else 
+           dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+       }
+   }
+   return dp[x][y];
+        // return f(x-1,y-1,s1,s2,dp);
     }
 };
 
