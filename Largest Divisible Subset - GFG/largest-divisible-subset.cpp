c92413e89604a55a6be38ed37551f6e7d47af617
KUMAR RISHAB <78472503+rishab0902@users.noreply.github.com>
2023-03-28 20:38:45 +0530

    Added solution - LeetHub

---
+++ Largest Divisible Subset - GFG/largest-divisible-subset.cpp
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> LargestSubset(int n, vector<int>& arr) {
+        // Code here
+        // int n = arr.size();
+    
+    //sort the array
+    
+    sort(arr.begin(), arr.end());
+
+    vector<int> dp(n,1);
+    vector<int> hash(n,1);
+    
+    for(int i=0; i<=n-1; i++){
+        
+        hash[i] = i; // initializing with current index
+        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+            
+            if(arr[i]%arr[prev_index] == 0 && 1 + dp[prev_index] > dp[i]){
+                dp[i] = 1 + dp[prev_index];
+                hash[i] = prev_index;
+            }
+        }
+    }
+    
+    int ans = -1;
+    int lastIndex =-1;
+    
+    for(int i=0; i<=n-1; i++){
+        if(dp[i]> ans){
+            ans = dp[i];
+            lastIndex = i;
+        }
+    }
+    
+    vector<int> temp;
+    temp.push_back(arr[lastIndex]);
+    
+    while(hash[lastIndex] != lastIndex){ // till not reach the initialization value
+        lastIndex = hash[lastIndex];
+        temp.push_back(arr[lastIndex]);    
+    }
+    
+    // reverse the array 
+    reverse(temp.begin(),temp.end());
+
+    
+    return temp;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr;
+
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            arr.push_back(x);
+        }
+
+        Solution obj;
+        vector<int> res = obj.LargestSubset(n, arr);
+        int isValidSeq = 1, sz = res.size();
+        for (int i = 0; i < sz; i++) {
+            for (int j = i + 1; j < sz; j++) {
+                if ((res[i] % res[j]) == 0 || (res[j] % res[i]) == 0)
+                    continue;
+                else {
+                    isValidSeq = 0;
+                    break;
+                }
+            }
+        }
+        cout << isValidSeq << " " << sz << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
