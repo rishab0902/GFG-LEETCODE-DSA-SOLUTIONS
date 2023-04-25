344add06ebf88ae28b44ca0de5ee91ab9543449a
KUMAR RISHAB <78472503+rishab0902@users.noreply.github.com>
2023-04-25 17:38:46 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.cpp
+++ Replace O's with X's - GFG/replace-os-with-xs.cpp
@@ -25,71 +25,32 @@ public:
     vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
     {
         // code here
-        // int delrow[]={-1,0,+1,0};
-        // int delcol[]={0,+1,0,-1};
-        // vector<vector<int>> vis(n,vector<int>(m,0));
-        // for(int j=0;j<m;j++){
-        //     if(!vis[0][j]&&mat[0][j]=='O'){
-        //         dfs(0,j,vis,mat,delrow,delcol);
-        //     }
-        //     if(!vis[n-1][j] && mat[n-1][j]=='O'){
-        //          dfs(n-1,j,vis,mat,delrow,delcol);
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     if(!vis[i][0]&&mat[i][0]=='O'){
-        //         dfs(i,0,vis,mat,delrow,delcol);
-        //     }
-        //     if(!vis[i][m-1] && mat[i][m-1]=='O'){
-        //          dfs(i,n-1,vis,mat,delrow,delcol);
-        //     }
-        // }
-        
-        //  for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         if(!vis[i][j] && mat[i][j]=='O')mat[i][j]='X';
-        //     }
-        // }
-        // return mat;
-           int delrow[] = {-1, 0, +1, 0};
-        int delcol[] = {0, 1, 0, -1}; 
-        vector<vector<int>> vis(n, vector<int>(m,0)); 
-        // traverse first row and last row 
-        for(int j = 0 ; j<m;j++) {
-            // check for unvisited Os in the boundary rows
-            // first row 
-            if(!vis[0][j] && mat[0][j] == 'O') {
-                dfs(0, j, vis, mat, delrow, delcol); 
+        int delrow[]={-1,0,+1,0};
+        int delcol[]={0,+1,0,-1};
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int j=0;j<m;j++){
+            if(!vis[0][j]&&mat[0][j]=='O'){
+                dfs(0,j,vis,mat,delrow,delcol);
             }
-            
-            // last row 
-            if(!vis[n-1][j] && mat[n-1][j] == 'O') {
-                dfs(n-1,j,vis,mat, delrow, delcol); 
+            if(!vis[n-1][j] && mat[n-1][j]=='O'){
+                 dfs(n-1,j,vis,mat,delrow,delcol);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(!vis[i][0]&&mat[i][0]=='O'){
+                dfs(i,0,vis,mat,delrow,delcol);
+            }
+            if(!vis[i][m-1] && mat[i][m-1]=='O'){
+                 dfs(i,m-1,vis,mat,delrow,delcol);
             }
         }
         
-        for(int i = 0;i<n;i++) {
-            // check for unvisited Os in the boundary columns
-            // first column 
-            if(!vis[i][0] && mat[i][0] == 'O') {
-                dfs(i, 0, vis, mat, delrow, delcol); 
-            }
-            
-            // last column
-            if(!vis[i][m-1] && mat[i][m-1] == 'O') {
-                dfs(i, m-1, vis, mat, delrow, delcol); 
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && mat[i][j]=='O')mat[i][j]='X';
             }
         }
-        
-        // if unvisited O then convert to X
-        for(int i = 0;i<n;i++) {
-            for(int j= 0 ;j<m;j++) {
-                if(!vis[i][j] && mat[i][j] == 'O') 
-                    mat[i][j] = 'X'; 
-            }
-        }
-        
-        return mat; 
+        return mat;
     }
 };
 
