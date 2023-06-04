e945c238b1b6f32c91adb94eead83742bbc425f8
KUMAR RISHAB <78472503+rishab0902@users.noreply.github.com>
2023-06-04 20:12:28 +0530

    Added solution - LeetHub

---
+++ Word Ladder II - GFG/word-ladder-ii.cpp
@@ -0,0 +1,113 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
+        // code here
+        queue<vector<string>> q;
+        q.push({beginWord});
+        unordered_set<string> st(wordList.begin(),wordList.end());
+        // st.erase(startWord);
+        vector<string> usedOnLevel;
+        usedOnLevel.push_back(beginWord);
+        int level=0;
+        vector<vector<string>> ans;
+        
+        while(!q.empty()){
+            vector<string> vec =q.front();
+            q.pop();
+            if(vec.size()>level){
+                level++;
+                for(auto it:usedOnLevel){
+                    st.erase(it);
+                }
+            }
+            string word = vec.back();
+             if (word == endWord)
+            {
+                // the first sequence where we reached end
+                if (ans.size() == 0)
+                {
+                    ans.push_back(vec);
+                }
+                else if (ans[0].size() == vec.size())
+                {
+                    ans.push_back(vec);
+                }
+            }
+            for (int i = 0; i < word.size(); i++)
+            {   
+                // Now, replace each character of ‘word’ with char
+                // from a-z then check if ‘word’ exists in wordList.
+                char original = word[i];
+                for (char c = 'a'; c <= 'z'; c++)
+                {
+                    word[i] = c;
+                    if (st.count(word) > 0)
+                    { 
+                        // Check if the word is present in the wordList and
+                        // push the word along with the new sequence in the queue.
+                        vec.push_back(word);
+                        q.push(vec);
+                        // mark as visited on the level
+                        usedOnLevel.push_back(word);
+                        vec.pop_back();
+                    }
+                }
+                word[i] = original;
+            }
+            }
+            return ans;
+       
+    }
+};
+
+//{ Driver Code Starts.
+
+bool comp(vector<string> a, vector<string> b)
+{
+    string x = "", y = "";
+    for(string i: a)
+        x += i;
+    for(string i: b)
+        y += i;
+    
+    return x<y;
+}
+
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<string>wordList(n);
+		for(int i = 0; i < n; i++)cin >> wordList[i];
+		string startWord, targetWord;
+		cin >> startWord >> targetWord;
+		Solution obj;
+		vector<vector<string>> ans = obj.findSequences(startWord, targetWord, wordList);
+		if(ans.size()==0)
+		    cout<<-1<<endl;
+		else
+		{
+		    sort(ans.begin(), ans.end(), comp);
+            for(int i=0; i<ans.size(); i++)
+            {
+                for(int j=0; j<ans[i].size(); j++)
+                {
+                    cout<<ans[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+		}
+	}
+	return 0;
+}
+// } Driver Code Ends
